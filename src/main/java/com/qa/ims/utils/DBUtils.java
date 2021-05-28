9ddeb1d7dae2fb8392325e9c8eabba9c6e6bed3d
Your Name <skeletoro@github.com>
2021-05-28 06:37:11 -0700

    zh adding documents folder updating db connection

--- src/main/java/com/qa/ims/utils/DBUtils.java
+++ src/main/java/com/qa/ims/utils/DBUtils.java
@@ -30,9 +30,9 @@ public class DBUtils {
 		} catch (Exception e) {
 			LOGGER.error(e);
 		}
-		this.dbUrl = dbProps.getProperty("db.url", "");
-		this.dbUser = dbProps.getProperty("db.user", "");
-		this.dbPassword = dbProps.getProperty("db.password", "");
+		this.dbUrl = dbProps.getProperty("db.url", "jdbc:mysql://localhost:3306/ims");
+		this.dbUser = dbProps.getProperty("db.user", "root");
+		this.dbPassword = dbProps.getProperty("db.password", "root");
 	}
 
 	public DBUtils() {
