cbfa616f797d3cb706d74454326d7c038d1054bc
Your Name <skeletoro@github.com>
2021-06-01 06:56:52 -0700

    adding testing classes trying out a test

---
+++ src/test/java/com/qa/ims/controllers/ItemControllerTest.java
@@ -0,0 +1,98 @@
+package com.qa.ims.controllers;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.qa.ims.controller.ItemController;
+import com.qa.ims.persistence.dao.ItemDAO;
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.Utils;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ItemControllerTest {
+	
+	@Mock
+	private Utils utils;
+	
+	@Mock
+	private ItemDAO dao;
+	
+	@InjectMocks
+	private ItemController controller;
+	
+	@Test 
+	public void testCreate() {
+		final String name = "ajax dish soap";
+		final Double value = 2.50 ;
+		final Item created = new Item (name,value);
+
+		Mockito.when(utils.getString()).thenReturn(name);
+		Mockito.when(utils.getDouble()).thenReturn(value);
+		Mockito.when(dao.create(created)).thenReturn(created);
+		
+		assertEquals(created, controller.create());
+		
+		Mockito.verify(utils, Mockito.times(1)).getString();
+		Mockito.verify(utils, Mockito.times(1)).getDouble();
+		Mockito.verify(dao,Mockito.times(1)).create(created);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
